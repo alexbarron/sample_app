ada98e838c95d605559f3793fe49a137b11f88b5
Alex Barron <alex@abarron.com>
2023-08-09 15:30:50 +0200

    Finish user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "should not accept invalid users" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name: "",
+        email: "email@invalid",
+        password: "f",
+        password_confirmation: "b"
+      }}
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'li', "Password is too short (minimum is 6 characters)"
+  end
+
+  test "should accept valid users" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        name: "My Name",
+        email: "email@valid.com",
+        password: "fyodor",
+        password_confirmation: "fyodor"
+      }}
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+    end
+  end
+end
