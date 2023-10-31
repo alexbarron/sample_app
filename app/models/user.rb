d39859e9489556e381d65c221beb65a290a08d2f
Alex Barron <alex@abarron.com>
2023-10-31 15:38:07 +0100

    Add account activation controller and model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,9 +52,21 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    # Returns true if the given token matches the digest.
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+    end
+
+    # Activates an account.
+    def activate
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
     end
 
     def feed
@@ -76,8 +88,9 @@ class User < ApplicationRecord
     
     private
 
+    # Creates and assigns the activation token and digest.
         def create_activation_digest
-            self.activation_token = User.new_token
+            self.activation_token  = User.new_token
             self.activation_digest = User.digest(activation_token)
         end
 end
